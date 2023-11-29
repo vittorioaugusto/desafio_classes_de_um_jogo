c61d1803af656fddd1c76c71b130ada6460de917
vittorioaugusto <vi.aborges47@gmail.com>
2023-11-29 16:19:46 -0300

    update

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 class Heroi {
-    constructor(nome, idade, tipo) {
-      this.nome = nome;
-      this.idade = idade;
-      this.tipo = tipo;
-    }
 
-    atacar() {
-      let ataque = this.obterAtaque();
-      console.log(`O ${this.tipo} atacou usando ${ataque}`);
-    }
-  
-    obterAtaque() {
-      switch (this.tipo) {
-        case 'mago':
-          return 'magia';
-        case 'guerreiro':
-          return 'espada';
-        case 'monge':
-          return 'artes marciais';
-        case 'ninja':
-          return 'shurikens';
-        default:
-          return 'ataque indefinido';
-      }
-    }
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
   }
 
-  const heroi1 = new Heroi('Gandalf', 1000, 'mago');
-  heroi1.atacar(); 
-  
-  const heroi2 = new Heroi('Aragorn', 35, 'guerreiro');
-  heroi2.atacar();
-  
-  const heroi3 = new Heroi('Bruce Lee', 80, 'monge');
-  heroi3.atacar(); 
-  
-  const heroi4 = new Heroi('Hanzo', 25, 'ninja');
-  heroi4.atacar();
+  atacar() {
+    let ataque;
+
+    switch (this.tipo) {
+      case 'mago':
+        ataque = 'magia';
+        break;
+      case 'guerreiro':
+        ataque = 'espada';
+        break;
+      case 'monge':
+        ataque = 'artes marciais';
+        break;
+      case 'ninja':
+        ataque = 'shurikens';
+        break;
+      default:
+        ataque = 'ataque desconhecido';
+    }
+
+    console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`);
+  }
+}
+
+const Heroi1 = new Heroi('Aragorn', 30, 'guerreiro');
+Heroi1.atacar();
+
+const Heroi2 = new Heroi('Gandalf', 150, 'mago');
+Heroi2.atacar();
+
+const Heroi3 = new Heroi('Zelda', 700, 'guerreiro');
+Heroi3.atacar();
